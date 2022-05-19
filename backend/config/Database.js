1996fb6d0ddf7af632057d23c8b5e17a6f73aa77
Fiki Aviantono <56351437+fikia13@users.noreply.github.com>
2022-05-19 13:41:13 +0700

    Delete backend directory

--- backend/config/Database.js
+++
@@ -1,8 +0,0 @@
-import {Sequelize} from "sequelize";
-
-const db = new Sequelize('crud_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql' /*mysql' | 'mariadb' | 'postgres' | 'mssql' */
-  });
-
-export default db;
